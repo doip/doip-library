220d8fb7dd5f63b10f3d265b6de85fded6f8de63
Marco Wehnert <marco.wehnert@gmx.de>
2020-02-19 18:02:22 +0100

    Added file doip.library.util.StringConstants.java

---
+++ src/main/java/doip/library/util/StringConstants.java
@@ -0,0 +1,34 @@
+package doip.library.util;
+
+public class StringConstants {
+	
+	/**
+	 * String with 78 characters '-'
+	 */
+	public static final String LINE  = "------------------------------------------------------------------------------";
+	
+	/**
+	 * String with 78 characters '#'
+	 */
+	public static final String FENCE = "##############################################################################";
+	
+	/**
+	 * String with 78 characters '='
+	 */
+	public static final String WALL  = "==============================================================================";
+	
+	/**
+	 * String with 36 characters "-"
+	 */
+	public static final String SHORTLINE  = "------------------------------------";
+	
+	/**
+	 * String with 36 characters '#'
+	 */
+	public static final String SHORTFENCE = "####################################";
+	
+	/**
+	 * String with 36 characters '='
+	 */
+	public static final String SHORTWALL  = "====================================";
+}
